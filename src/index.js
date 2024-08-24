17367211d67e421e139f93de3f9011fbafeaacc5
wtwo1529 <003axelw@gmail.com>
2024-08-24 14:52:36 +0800

    Split webpack config into dev and prod with webpack-merge, added scripts

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import './style.css';
 import logo from './components/homepage/logo-1.png'
 import orderNowPhoto from './components/homepage/order_now.png';
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+  }
+  
 window.onload = () => {
     setNavBtns();
     let orderBtn = document.querySelector('#order-now');
